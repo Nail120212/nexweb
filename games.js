5cb89b6047162d9d4a2ae437a79fb131565f2f89
Nail120212 <ianangeloi122@gmail.com>
2026-07-19 08:14:31 +0800

    Add files via upload

---
+++ games.js
@@ -0,0 +1,7 @@
+var GAMES = [
+  {
+    gamename: "Diesel N Steel",
+    gamelogo: "https://tr.rbxcdn.com/180DAY-a79b1f2af4319127d5a9e765c3b58d29/768/432/Image/Webp/noFilter",
+    gamelink: "https://www.roblox.com/games/15532962292/Diesel-n-Steel"
+  }
+];
